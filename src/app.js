b3dc7eef67b3df8b7d72d0b7d2f90320aab40413
NayZBySodium <noreply@nayzbysodium.com>
2022-02-07 20:47:11 +0100

    Vulnerability CSRF patch

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const session = require("express-session");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const csurf = require("csurf");
 const MongoStore = require(`connect-mongo`);
 
 const authRoutes = require("./routes/auth.routes");
@@ -9,11 +10,11 @@ const userRoutes = require("./routes/user.routes");
 
 const app = express();
 
-// require("dotenv").config({ path: ".ENV" });
+require("dotenv").config({ path: ".ENV" });
 require("./database/mongodb");
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(session(
     {
@@ -25,7 +26,12 @@ app.use(session(
     }
 ));
 
-app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/auth", csurf({ cookie: true }), authRoutes);
+app.use("/api/v1/users", csurf({ cookie: true }), userRoutes);
+
+// get an csrf token
+app.get("/api/v1/csrf", csurf({ cookie: true }), (req, res) => {
+    res.status(200).json({ csrfToken: req.csrfToken() });
+});
 
 app.listen((process.env.PORT || 80), () => console.log("Serveur démarré sur le port : " + (process.env.PORT || 80) + "."));
